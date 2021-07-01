e5f760868d6641471fd6cb5c8c05995fabe70816
Yara Regina <yara.regina@hotmail.com>
2021-06-30 22:29:01 -0300

    Projeto com Entity Framework + MySQL

---
+++ Herois/Herois/Model/HeroiBatalha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Herois.Model
+{
+    public class HeroiBatalha
+    {
+        public int HeroId { get; set; }
+        public Heroi Heroi { get; set; }
+        public int BatalhaId { get; set; }
+       
+        public Batalha Batalha { get; set; }
+
+    }
+}
---
+++ Herois/Herois/Startup.cs
@@ -0,0 +1,59 @@
+using Herois.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Herois
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<HeroiContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("HeroiContext"),
+                       new MySqlServerVersion(new Version(8, 0, 11))
+                        ));
+
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
